e77823a3904612a03d1bf9413f50e3541a833bbc
Balanced02 <adebalanced02@gmail.com>
2017-11-18 22:38:12 +0100

    editing profile and user details and school details

---
+++ dist/models/Course.js
@@ -0,0 +1,52 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _v = require('uuid/v4');
+
+var _v2 = _interopRequireDefault(_v);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var courseSchema = new _mongoose2.default.Schema({
+  sid: {
+    type: String,
+    required: true,
+    default: _v2.default
+  },
+  courseName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  schoolId: {
+    type: String,
+    required: true
+  },
+  courseCode: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  minAttendance: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+exports.default = _mongoose2.default.model('Course', courseSchema);
+//# sourceMappingURL=Course.js.map
